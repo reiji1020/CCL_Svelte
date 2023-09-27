6f895696f7945727a34180331aec5b84cae8e81b
reiji1020 <sk@reiji1020.info>
2023-09-27 23:57:09 +0900

    [update]add mainview

---
+++ src/components/Activity.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    /**
+     * メインビュー内で使用する活動内容説明用コンポーネント
+     * */
+    export let titleText: string /* タイトル文字列 */
+    export let explainText: string /* 説明文 */
+
+</script>
+
+<div class="activity">
+    <div class="title">
+        <h2>{titleText}</h2>
+    </div>
+    <div class="explain">
+        <p>{explainText}</p>
+    </div>
+</div>
+
+<style>
+    h2 {
+        font-size: 32px;
+        color: #FFF;
+        margin: 0;
+    }
+
+    p {
+        margin: 12px 0;
+    }
+
+    .activity {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .title {
+        border-radius: 27.5px;
+        background: #ED3B7D;
+        width: fit-content;
+        padding: 4px 16px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .explain {
+        font-size: 20px;
+        color: #4B4B4B;
+        margin: 0 16px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Header from "../components/Header.svelte";
+    import Activity from "../components/Activity.svelte";
+</script>
+
+<Header />
+<main>
+    <div class="main-view">
+        <div class="column">
+            <div class="catch-copy">
+                <h1>テクノロジーと<br />ハンドメイドで<br/>世の中を豊かにする</h1>
+            </div>
+        </div>
+        <div class="column">
+            <div class="activities">
+                <Activity titleText="技術書執筆" explainText="同人技術書を執筆して、世の中のトレンド技術の情報発信を行っています。" />
+                <Activity titleText="ハッカソン" explainText="各種ハッカソンに参加して、世の中に「新しくて面白い」を届けます。" />
+                <Activity titleText="ハンドメイド作品販売" explainText="ハンドメイドアクセサリー・ハンドメイド雑貨など、一点ものの服飾品を製作・販売しています。" />
+            </div>
+        </div>
+    </div>
+</main>
+
+<style>
+    h1 {
+        font-size: 48px;
+        color: #ED3B7D;
+    }
+
+    .main-view {
+        height: 450px;
+        background: linear-gradient(180deg, rgba(237, 145, 38, 0.50) 50%, rgba(255, 255, 255, 0.00) 100%);
+        display: flex;
+    }
+    .column {
+        width: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .activities {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
