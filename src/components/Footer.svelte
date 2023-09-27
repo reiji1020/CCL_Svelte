b28c7b7dd59cfb9262b94089b0efd076c2be346e
reiji1020 <sk@reiji1020.info>
2023-09-28 02:34:49 +0900

    [update]かんせい

---
+++ src/components/Footer.svelte
@@ -0,0 +1,32 @@
+<script>
+/* TypeScriptで記述する部分 */
+
+</script>
+
+<!--HTML構文で記述する部分-->
+<footer>
+    <div class="copylight">
+        <p>Copyright © 2017-2023 CANDY CHUPS Lab. All Rights Reserved.</p>
+    </div>
+</footer>
+
+<style>
+/* スタイルシート部分 */
+footer {
+    background: #ED3B7D;
+    height: 54px;
+}
+
+p {
+    color: #FFF;
+    font-size: 12px;
+}
+
+.copylight {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+}
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import Header from "../components/Header.svelte";
     import Activity from "../components/Activity.svelte";
+    import Footer from "../components/Footer.svelte";
 </script>
 
 <Header />
@@ -19,12 +20,34 @@
             </div>
         </div>
     </div>
+    <div class="sub-view">
+        <div class="column">
+            <img class="thumbnail" src="thumb.jpg" alt="thumbnail" />
+        </div>
+            <div class="column">
+                <div class="profiles">
+                    <div class="profile-title">
+                        <h2>Member</h2>
+                    </div>
+                    <p class="profile">Name:れいじ</p>
+                    <p class="profile">X:@reiji1020</p>
+                    <p class="profile">Circle Owner/Web Developer/Handmade Creator</p>
+                </div>
+            </div>
+    </div>
 </main>
+<Footer />
 
 <style>
     h1 {
         font-size: 48px;
         color: #ED3B7D;
+    }
+
+    h2 {
+        font-size: 32px;
+        color: #FFF;
+        margin: 0;
     }
 
     .main-view {
@@ -42,5 +65,40 @@
     .activities {
         display: flex;
         flex-direction: column;
+        width: 768px;
+    }
+
+    .sub-view {
+        height: 400px;
+        background: rgba(32, 137, 234, 0.50);
+        display: flex;
+    }
+
+    .profiles {
+        display: flex;
+        flex-direction: column;
+        width: 768px;
+    }
+
+    .thumbnail {
+        width: 256px;
+        height: 256px;
+        border-radius: 50px;
+    }
+
+    .profile-title {
+        border-radius: 27.5px;
+        background: #2089EA;
+        width: 250px;
+        height: 60px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .profile {
+        color: #4B4B4B;
+        font-size: 24px;
+        font-weight: 700;
     }
 </style>
